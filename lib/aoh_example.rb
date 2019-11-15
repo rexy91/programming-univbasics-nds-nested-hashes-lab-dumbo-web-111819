54e5f1f051703e4e2e190031dd5a468750a90d7c
Yu Hao Ye <rxye1991@gmail.com>
2019-11-15 02:42:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,6 +22,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
+  
   array = [
     { name:"Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
@@ -29,6 +30,7 @@ def literal_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
+    
 end
 
 def aoh_lookup(aoh, row, key)
